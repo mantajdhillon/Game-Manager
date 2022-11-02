c6407d59bae1ae4aa5cf9e94fb67058bb674e151
Olive Tempel <okt@sfu.ca>
2022-11-01 19:42:54 -0700

    implement navigation of back buttons and achievement message pop-up.
    Back buttons navigate back to previous activity on stack, and message displays prior to achievement activity launch, prompting user to enter a valid number of players.

--- app/src/main/java/ca/cmpt276/neon_coopachievement/GameConfigActivity.java
+++ app/src/main/java/ca/cmpt276/neon_coopachievement/GameConfigActivity.java
@@ -1,10 +1,12 @@
 package ca.cmpt276.neon_coopachievement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -41,5 +43,22 @@ public class GameConfigActivity extends AppCompatActivity {
         deleteBtn.setOnClickListener(view -> Toast.makeText(this,
                 "Should delete game",
                 Toast.LENGTH_SHORT).show());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
