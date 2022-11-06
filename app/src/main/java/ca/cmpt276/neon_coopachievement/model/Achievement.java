008443fbd793a9dddcdba3e748f7fec0704d3efa
Mantaj <mkd21@sfu.ca>
2022-11-06 15:04:30 -0800

    make Achievement have a toString

--- app/src/main/java/ca/cmpt276/neon_coopachievement/model/Achievement.java
+++ app/src/main/java/ca/cmpt276/neon_coopachievement/model/Achievement.java
@@ -1,16 +1,22 @@
 package ca.cmpt276.neon_coopachievement.model;
 
+import java.util.Arrays;
+
 public class Achievement {
     private static final byte MAX_ACHIEVEMENTS = 10;
 
     private final double[] achievementBoundaries;
     private final String[] achievementNames;
+    int numPlayers;
 
     public Achievement(int low, int high, int numPlayers) {
         if (low > high) {
             throw new RuntimeException("Low score can not be greater than high score");
         }
-
+        if (numPlayers < 0) {
+            throw new IllegalArgumentException("Invalid number of players");
+        }
+        this.numPlayers = numPlayers;
         this.achievementNames = new String[]{
                 "Achievement Level 1", "Achievement Level 2",
                 "Achievement Level 3", "Achievement Level 4", "Achievement Level 5",
@@ -55,6 +61,15 @@ public class Achievement {
         return rank;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("");
+        for (int i = 0; i < achievementBoundaries.length; i++) {
+            s.append(achievementNames[i]).append(" for a cooperative score less than or equal to ").append(achievementBoundaries[i]).append("! /n");
+        }
+        s.append(achievementNames[MAX_ACHIEVEMENTS - 1]).append(" for a cooperative score greater than ").append(achievementBoundaries[MAX_ACHIEVEMENTS - 2]).append("! /n");
+        return s.toString();
+    }
 }
 
 
