25b35fb4a09e603974b1aee67670bb1def5aa0da
Mekdim Dereje <mda89@sfu.ca>
2022-11-07 12:06:21 -0800

    Clean up code and remove test logs

--- app/src/main/java/ca/cmpt276/neon_coopachievement/CategorySaver.java
+++ app/src/main/java/ca/cmpt276/neon_coopachievement/CategorySaver.java
@@ -2,29 +2,15 @@ package ca.cmpt276.neon_coopachievement;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
-
-import java.lang.reflect.Type;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 import ca.cmpt276.neon_coopachievement.model.GameCategory;
 
 public class CategorySaver {
 
     private static final String KEY = "GAME_CATEGORY_JSON_KEY";
-//    private static final String INVALID_PARSE_VAL = "FAILED_TO_PARSE";
-//    private static final String INVALID_PARSE_VAL = "";
+    private static final String INVALID_PARSE_VAL = "";
 
     private GameCategory gameCategory;
     private final SharedPreferences preference;
@@ -38,20 +24,17 @@ public class CategorySaver {
         this.editor = preference.edit();
 
         getSharedPreferenceData();
-
-        Log.i("GameCategorySaver Constructor", "Game category was constructed. GameCategorySaver(Context context) successful");
     }
 
     // Retrieve game category JSON from SharedPreference
     private void getSharedPreferenceData() {
-        String gameCategoryJson = preference.getString(KEY, "");
+        String gameCategoryJson = preference.getString(KEY, INVALID_PARSE_VAL);
 
         // Update object if gameCategory was successfully parsed
-        if (!gameCategoryJson.isEmpty()) {
+        if (!gameCategoryJson.equals(INVALID_PARSE_VAL)) {
             gameCategory = gson.fromJson(gameCategoryJson, GameCategory.class);
             gameCategory.setInstance();
         }
-        Log.i("Get Shared Preference Data", "Game category was put in SharedPreferences. getSharedPreferenceData() successful");
     }
 
     // Serialize game category (store it as a json)
@@ -61,6 +44,5 @@ public class CategorySaver {
         // Store in SharedPreference
         editor.putString(KEY, gameCategoryJson);
         editor.apply();
-        Log.i("Save Data", "Game category was saved. saveData() successful");
     }
 }
