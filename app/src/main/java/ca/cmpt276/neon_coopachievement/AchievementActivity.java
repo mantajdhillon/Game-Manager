a6a7ccc6699197894a6a4356364e57a04eb3bcc5
Mantaj <mkd21@sfu.ca>
2022-11-06 20:35:13 -0800

    [GROUP COMMIT] Achievements screen edited
    complex list view shows images and description of the achievement levels

--- app/src/main/java/ca/cmpt276/neon_coopachievement/AchievementActivity.java
+++ app/src/main/java/ca/cmpt276/neon_coopachievement/AchievementActivity.java
@@ -1,6 +1,7 @@
 package ca.cmpt276.neon_coopachievement;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,13 +10,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ca.cmpt276.neon_coopachievement.model.Achievement;
 import ca.cmpt276.neon_coopachievement.model.GameCategory;
-import ca.cmpt276.neon_coopachievement.model.GameManager;
 
 public class AchievementActivity extends AppCompatActivity {
 
@@ -26,7 +32,7 @@ public class AchievementActivity extends AppCompatActivity {
 
     GameCategory gameCategory = GameCategory.getInstance();
 
-    Achievement achievements;
+    List<AchievementListElement> listAchievements = new ArrayList<AchievementListElement>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,21 +44,26 @@ public class AchievementActivity extends AppCompatActivity {
         ab.setTitle(R.string.achievement_activity_title);
         ab.setDisplayHomeAsUpEnabled(true);
 
+        populateAchievementsList();
+        populateListView();
 
-        achievements = new Achievement(getPoorScore(), getGoodScore(), getNumPlayers());
+    }
 
-        String[] ranks = new String[MAX_ACHIEVEMENTS];
+
+    private void populateAchievementsList() {
+        Achievement achievements = new Achievement(getPoorScore(), getGoodScore(), getNumPlayers());
         for (int i = 0; i < MAX_ACHIEVEMENTS; i++) {
-            ranks[i] = achievements.getAchievementString(i);
+            String filename = "level_" + (i+1);
+            int id = getResources().getIdentifier(filename, "drawable", this.getPackageName());
+            listAchievements.add(new AchievementListElement
+                     (achievements.getAchievementString(i), id));
         }
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this,
-                R.layout.items,
-                ranks);
-
-        ListView achievements = findViewById(R.id.achievementList);
-        achievements.setAdapter(adapter);
+    private void populateListView() {
+        ArrayAdapter<AchievementListElement> adapter = new MyListAdapter();
+        ListView list = findViewById(R.id.achievementList);
+        list.setAdapter(adapter);
     }
 
     @Override
@@ -95,5 +106,41 @@ public class AchievementActivity extends AppCompatActivity {
     private int getGoodScore() {
         Intent intent = getIntent();
         return intent.getIntExtra(GOOD_SCORE, -1);
+    }
+
+    private class AchievementListElement {
+        public String description;
+        public int iconId;
+
+        public AchievementListElement(String description, int iconId) {
+            this.description = description;
+            this.iconId = iconId;
+        }
+    }
+
+    private class MyListAdapter extends ArrayAdapter<AchievementListElement> {
+        public MyListAdapter() {
+            super(AchievementActivity.this, R.layout.achievements_list, listAchievements);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View achievementView = convertView;
+            if (achievementView == null) {
+                achievementView = getLayoutInflater().inflate
+                        (R.layout.achievements_list, parent, false);
+            }
+
+            AchievementListElement currentElement= listAchievements.get(position);
+
+            ImageView icon = achievementView.findViewById(R.id.achievement_icon);
+            icon.setImageResource(currentElement.iconId);
+
+            TextView description = achievementView.findViewById(R.id.tvAchievementsDescription);
+            description.setText(currentElement.description);
+
+            return achievementView;
+        }
     }
 }
