625186899162d373335e21f37d2e566307717e41
Mekdim Dereje <mda89@sfu.ca>
2022-11-07 21:44:38 -0800

    Update GameCategory class

--- app/src/main/java/ca/cmpt276/neon_coopachievement/model/GameCategory.java
+++ app/src/main/java/ca/cmpt276/neon_coopachievement/model/GameCategory.java
@@ -1,25 +1,21 @@
 package ca.cmpt276.neon_coopachievement.model;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 /**
-    GameCategory Class
-    Description: Holds the Game Managers for several different game types, such as Poker or Blackjack
-                - Singleton design: returns an instance of the GameCategory
-                - GameManagers are held in an ArrayList
-                - GameManagers can only be accessed through the GameCategory instance
-*/
+ * GameCategory Class
+ * <p>
+ * Description: Holds the Game Managers for several different game types, such as Poker or Blackjack
+ * - Singleton design: returns an instance of the GameCategory
+ * - GameManagers are held in an ArrayList
+ * - GameManagers can only be accessed through the GameCategory instance
+ */
 public class GameCategory {
     private static GameCategory instance;
     private final ArrayList<GameManager> gameManagerList;
 
     private GameCategory() {
         gameManagerList = new ArrayList<>();
-    }
-
-    public int getSize() {
-        return gameManagerList.size();
     }
 
     public static GameCategory getInstance() {
@@ -29,30 +25,17 @@ public class GameCategory {
         return instance;
     }
 
-    // Update the current instance to "this" object
+    // Update the current instance
     public void setInstance() {
         instance = this;
     }
 
-    // getGameManager: searches through GameManagersStored, if it contains the object, for the object in question
-    public GameManager getGameManager(GameManager toGet) {
-        boolean found = this.gameManagerList.contains(toGet);
-        if (found) {
-            for (int i = 0; i < gameManagerList.size(); i++) {
-                if (this.gameManagerList.get(i) == toGet) {
-                    return this.gameManagerList.get(i);
-                }
-            }
-        }
-        throw new InvalidParameterException("Game not found!");
+    public int size() {
+        return gameManagerList.size();
     }
 
     public GameManager getGameManager(int i) {
         return gameManagerList.get(i);
-    }
-
-    public String getGameManagerString(int i) {
-        return gameManagerList.get(i).toString();
     }
 
     public void addGameManager(GameManager toAdd) {
